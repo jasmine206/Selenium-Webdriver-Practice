b50423cf1bc6d5ca71033c7b3ea59b616c707c14
Jasmine Nguyen <jasmine@gotitapp.co>
2019-08-25 22:52:15 +0700

    - Put methods into classes - Find element by id or CSS selector - Update windows handling method - Add method to check if expert claims right question which is posted by API - Add method to check if expert saw asker's message which is sent by API

---
+++ practice/driver.py
@@ -0,0 +1,13 @@
+import os
+
+from selenium import webdriver
+
+
+class GetDriver:
+    def get_driver(self):
+        driver_chrome_location = "./chromedriver"
+        os.environ["webdriver.chrome.driver"] = driver_chrome_location
+        driver = webdriver.Chrome(driver_chrome_location)
+        baseUrl = "https://expert-excel.got-it.io/"
+        driver.get(baseUrl)
+        return driver
